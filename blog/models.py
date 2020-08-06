7ca4eebc01439b7356cdba78486017b3b3a4c85b
Lavlet <iberezin1500@gmail.com>
2020-08-06 07:46:58 +0300

    My first Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=300)
+	image = models.ImageField(upload_to='event_images/')
+	text = models.TextField()
+	date = models.DateTimeField()
+
+		
